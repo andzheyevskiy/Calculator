9ba2d9c5b8377fcc0ce37ff7401fc7da3100480e
andzheyevskiy <Igor_lifeking@hotmail.com>
2024-06-17 21:59:42 +0200

    Implemented Operator Buttons and Clear Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import { useState } from 'react'
 import buttonsJson from "./assets/buttons.json"
 import NumberButtons from "./assets/components/NumberButtons"
+import OperatorButtons from './assets/components/OperatorButtons'
+import ClearButton from './assets/components/ClearButton'
 
 function App() {
   const buttons = buttonsJson
 
   // App logic: each of the number for the operation will be saved in a state. The index is going to dictate on which of the state we operate:
   // index 0: firstNumber    index 1: Second number
-  // index is going to be 0 only on a clean operation; in chained operations we will always be index 1, since index 0 is going to be the result
   // when we do an operation, it takes in consideration operator(firstNumber,secondNumber), changes the value of the first number to the result
-  // and resets the second number and the operator
+  // and resets the second number.
   const [numberIndex, setNumberIndex] = useState("0")
   const [firstNumber, setFirstNumber] = useState("0")
   const [secondNumber, setSecondNumber] = useState("")
   const [operator, setOperator] = useState("")
-  let operatorSymbol;
-  operatorSymbol = operator? buttons.operators.find(e=> e.id==operator).display : ""
+  let operatorSymbol= operator? buttons.operators.find(e=> e.id==operator).display : ""
 
   return (
     <>
@@ -34,8 +34,28 @@ function App() {
                 secondNumber={secondNumber} setSecondNumber={setSecondNumber}
               />)}
           </section>
+          <section className='operators-wrapper'>
+            {buttons.operators.map(e => 
+              <OperatorButtons
+              key={e.button}
+              number={e}
+              index={numberIndex} setIndex={setNumberIndex}
+              firstNumber={firstNumber} setFirstNumber={setFirstNumber}
+              secondNumber={secondNumber} setSecondNumber={setSecondNumber}
+              operator={operator} setOperator={setOperator}
+              />)}
+          </section>
+          <section className='clear-btn'>
+            <ClearButton
+            key={buttons.clear.button}
+            number={buttons.clear}
+            setIndex={setNumberIndex}
+            setFirstNumber={setFirstNumber}
+            setSecondNumber={setSecondNumber}
+            setOperator={setOperator}
+              />
+          </section>
         </div>
-
       </div>
     </>
   )
